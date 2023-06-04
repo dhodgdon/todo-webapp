3d9317dc7e3f3643c1b3cc7390c7dfdd9d16f4f0
dhodgdon <hod20010@byui.edu>
2023-06-03 23:48:24 -0600

    Updated web app.

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# todo-webapp
+# Overview
+
+This is basic to-do list web app built on a full MERN stack.
+
+To run the application, open two terminal windows and navigate to the client and api directories respectively ('cd client' and 'cd api' if you are in the todo-webapp directory). Run 'npm start' in both of these terminals. Then navigate to http://localhost:3000 (if React hasn't already opened it for you). You are now free to make your own to-do list!
+
+I wrote this application to learn how to write a fullstack MERN application as well as update my web development skills.
+
+[Software Demo Video](http://youtube.link.goes.here)
+
+# Web Pages
+
+The web page allows you to create, complete, and delete tasks from your to-do list. These task items are created dynamically.
+
+I will add a login page to this application in the future.
+
+# Development Environment
+
+* Visual Studio Code
+* Git / GitHub
+* Programming languages: Javascript, CSS, HTML
+* Libraries: 
+    * Node
+    * Express
+    * React
+    * Mongoose
+
+# Useful Websites
+
+* [Build a FULL STACK app with React, Express, Node & Mongo MERN STACK](https://www.youtube.com/watch?v=R81g-2r6ynM&t=2634s)
+
+# Future Work
+
+* Add a login page
+* Update the color scheme for the page
+* Add user's name to welcome 
--- api/server.js
+++ api/server.js
@@ -49,3 +49,62 @@ app.get("/todo/complete/:id", async (req, res) => {
 })
 
 app.listen(3001, () => console.log("Server started on port 3001"));
+
+
+
+
+
+
+// -------------------------------------------------------------------------------------
+
+
+// const express = require('express');
+// const app = express();
+// const PORT = 3001;
+// const cors = require('cors');
+// require("dotenv").config();
+// const db = require("./db/connection");
+
+// app.use(express.json());
+// app.use(cors());
+
+// db
+//     .then(() => console.log("Connected to database"))
+//     .catch(console.error);
+
+// const Todo = require("./models/todoModel");
+
+// app.get("/todos", async (req, res) => {
+//     const todos = await Todo.find();
+
+//     res.json(todos);
+// })
+
+// app.post("/todo/new", (req, res) => {
+//     const todo = new Todo({
+//         text: req.body.text
+//     });
+
+//     todo.save();
+
+//     res.json(todo);
+// });
+
+// app.delete("/todo/delete/:id", async (req, res) => {
+//     const result = await Todo.findByIdAndDelete(req.params.id);
+
+//     res.json(result);
+// })
+
+// app.get("/todo/complete/:id", async (req, res) => {
+//     const todo = await Todo.findById(req.params.id);
+
+//     todo.complete = !todo.complete;
+
+//     todo.save();
+
+//     res.json(todo);
+// })
+
+// app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
